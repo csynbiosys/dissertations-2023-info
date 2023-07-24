67df0cc19b1476a831f53175645b179e86a3dd69
VegaQ <92881379+VegaQ@users.noreply.github.com>
2023-07-24 09:30:11 -0400

    Add files via upload

---
+++ Vega_Qi_whisky_project/other_websites/crawl.sh
@@ -0,0 +1,34 @@
+# python3 crawl_auction.py --run crawl --st 60000 --ed 70000 &
+# python3 crawl_auction.py --run crawl --st 70000 --ed 80000 &
+# python3 crawl_auction.py --run crawl --st 80000 --ed 90000 &
+# python3 crawl_auction.py --run crawl --st 90000 --ed 100000 &
+python3 crawl_auction.py --run crawl --st 100000 --ed 110000 &
+python3 crawl_auction.py --run crawl --st 110000 --ed 120000 &
+python3 crawl_auction.py --run crawl --st 120000 --ed 130000 &
+python3 crawl_auction.py --run crawl --st 130000 --ed 140000 &
+python3 crawl_auction.py --run crawl --st 140000 --ed 150000 &
+python3 crawl_auction.py --run crawl --st 150000 --ed 160000 &
+python3 crawl_auction.py --run crawl --st 160000 --ed 170000 &
+python3 crawl_auction.py --run crawl --st 170000 --ed 180000 &
+python3 crawl_auction.py --run crawl --st 180000 --ed 190000 &
+python3 crawl_auction.py --run crawl --st 190000 --ed 200000 &
+# python3 crawl_auction.py --run crawl --st 200000 --ed 210000 &
+# python3 crawl_auction.py --run crawl --st 210000 --ed 220000 &
+# python3 crawl_auction.py --run crawl --st 220000 --ed 230000 &
+# python3 crawl_auction.py --run crawl --st 230000 --ed 240000 &
+# python3 crawl_auction.py --run crawl --st 240000 --ed 250000 &
+# python3 crawl_auction.py --run crawl --st 250000 --ed 260000 &
+# python3 crawl_auction.py --run crawl --st 260000 --ed 270000 &
+# python3 crawl_auction.py --run crawl --st 270000 --ed 280000 &
+# python3 crawl_auction.py --run crawl --st 280000 --ed 290000 &
+# python3 crawl_auction.py --run crawl --st 290000 --ed 300000 &
+# python3 crawl_auction.py --run crawl --st 300000 --ed 310000 &
+# python3 crawl_auction.py --run crawl --st 310000 --ed 320000 &
+# python3 crawl_auction.py --run crawl --st 320000 --ed 330000 &
+# python3 crawl_auction.py --run crawl --st 330000 --ed 340000 &
+# python3 crawl_auction.py --run crawl --st 340000 --ed 350000 &
+# python3 crawl_auction.py --run crawl --st 350000 --ed 360000 &
+# python3 crawl_auction.py --run crawl --st 360000 --ed 370000 &
+# python3 crawl_auction.py --run crawl --st 370000 --ed 380000 &
+# python3 crawl_auction.py --run crawl --st 380000 --ed 390000 &
+# python3 crawl_auction.py --run crawl --st 390000 --ed 400000 &
